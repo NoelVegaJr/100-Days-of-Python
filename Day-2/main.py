47335a9a6cf90258329197b867ec9d4166475030
NoelVegaJr <noeljr126@gmail.com>
2022-05-30 03:23:16 -0500

    Removed unnecessary ()

--- Day-2/main.py
+++ Day-2/main.py
@@ -2,5 +2,5 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? "))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? ")) / 100
 people = int(input("How many people to split the bill? "))
-total_each = round((bill / people) * tip + (bill / people), 2)
+total_each = round(bill / people * tip + bill / people, 2)
 print(f"Each person should pay: ${total_each}")

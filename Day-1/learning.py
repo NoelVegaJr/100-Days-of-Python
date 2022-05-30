44bcaa2dc2d9dc25b026f24de53a418c15ae03c5
NoelVegaJr <noeljr126@gmail.com>
2022-05-30 02:04:50 -0500

    Band Generator

---
+++ Day-1/learning.py
@@ -0,0 +1,3 @@
+# Day 1 learninig
+
+print("Hello World!")
---
+++ Day-1/main.py
@@ -0,0 +1,6 @@
+print("Having trouble creating a band name?")
+
+city = input("Enter the name of the city you grew up in:\n")
+pet = input("Enter a pets name:\n")
+band = city + " " + pet
+print("Your new band name is " + band)

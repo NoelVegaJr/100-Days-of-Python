663aed1467dea97c85309c5b5ed33c77448f6e35
NoelVegaJr <noeljr126@gmail.com>
2022-06-07 23:42:42 -0500

    updated dealing of cards to the players

--- Day-11/main.py
+++ Day-11/main.py
@@ -5,20 +5,20 @@ from art import logo
 def deal():
     return random.choice([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] )
 
-def player_hit_phase(hand):
+def user_hit_phase(hand):
     current_hand = hand
     score = sum(current_hand)
     if score > 21:
         if 11 in current_hand:
             current_hand.remove(11)
             current_hand.append(1)
-            player_hit_phase(current_hand)
+            user_hit_phase(current_hand)
         return current_hand
     else:
         print(f"Your hand: {current_hand}")
         if input("Hit or Stay: ") == "hit":
             current_hand.append(deal())
-            return player_hit_phase(current_hand)
+            return user_hit_phase(current_hand)
         else:
             return current_hand
 
@@ -42,19 +42,22 @@ def computer_hit_phase(computer_hand, player_hand):
 
 def blackjack():
     print(logo)
-    computer_hand = [deal(), deal()]
-    print(f"Computers hand: {computer_hand[0]}")
+    user_cards = []
+    computer_cards = []
     
-    player_hand = player_hit_phase([deal(), deal()])
-    
-    
-    player_score  = sum(player_hand)
+    for _ in range(2):
+        user_cards.append(deal()) 
+        computer_cards.append(deal())
+
+    print(f"Computers hand: {computer_cards[0]}")
+    user_cards = user_hit_phase(user_cards)
+    player_score  = sum(user_cards)
     
     if player_score > 21:
         print("You bust :(")
     else:   
-        computer_hand = computer_hit_phase(computer_hand=computer_hand, player_hand=player_hand) 
-        computer_score = sum(computer_hand)
+        computer_cards = computer_hit_phase(computer_hand=computer_cards, player_hand=user_cards) 
+        computer_score = sum(computer_cards)
 
         if computer_score > 21:
             print("You win!")
@@ -63,7 +66,7 @@ def blackjack():
         else:
             print("You lose :(")
             
-    print(f"Your hand: {player_hand}\nComputers hand: {computer_hand}")
+    print(f"Your hand: {user_cards}\nComputers hand: {computer_cards}")
     
     if input("\nDo you want to play again 'y' for yes and 'n' for no.\n") == "y":
         blackjack()
